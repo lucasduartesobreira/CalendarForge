df844f411bf74b442dbbac1d9aa80c0671c55eb9
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-19 16:38:20 -0300

    feat(calendar): start draft of a calendar

---
+++ app/components/calendar/calendarWeek.tsx
@@ -0,0 +1,36 @@
+const range24 = Array.from(new Array(24));
+
+const DayBackground = () => {
+  return (
+    <div
+      className={`flex-none relative -top-[${
+        -1440 + 835.63
+      }px] h-[1440px] min-w-min grid grid-cols-1 bg-white text-black`}
+    >
+      {range24.map(() => {
+        // eslint-disable-next-line react/jsx-key
+        return <HourBackgroud />;
+      })}
+    </div>
+  );
+};
+
+const HourBackgroud = () => {
+  return <div className={`border-2 border-gray-600 h-${1440 / 24}`}>Hour</div>;
+};
+
+const CalendarWeek = ({ style }: { style: string }) => {
+  return (
+    <div className={`${style} grid grid-cols-7 overflow-clip`}>
+      <DayBackground />
+      <DayBackground />
+      <DayBackground />
+      <DayBackground />
+      <DayBackground />
+      <DayBackground />
+      <DayBackground />
+    </div>
+  );
+};
+
+export default CalendarWeek;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,25 @@
-import CreateEventButton, { CreateEventForm } from './components/createEvent/createEvent'
+import CalendarWeek from "./components/calendar/calendarWeek";
+import CreateEventButton, {
+  CreateEventForm,
+} from "./components/createEvent/createEvent";
 
 const Home = () => {
   return (
     <main className="flex flex-col gap-y-px min-h-screen items-center justify-between border-4 bg-black">
-      <div className='min-w-full flex-none w-12 h-12 border-2 border-red-500'>Navbar</div>
-      <div className='flex flex-auto h-full w-full border-4 border-red-700'>
-        <div className='flex-auto border-2 border-red-400 h-11/12'>Side Bar</div>
-        <div className='flex-auto w-8/12'>Calendar</div>
+      <div className="min-w-full flex-none w-12 h-12 border-2 border-red-500">
+        Navbar
       </div>
-      <CreateEventButton/>
+      <div className="flex flex-auto row h-full w-full">
+        <div className="flex-initial border-2 border-red-400 h-11/12 w-[15%]">
+          Side Bar
+        </div>
+        <div className="flex-none w-[85%]  overflow-clip">
+          <CalendarWeek style={"flex-none w-[100%] h-[100%]"} />
+        </div>
+      </div>
+      <CreateEventButton />
     </main>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
