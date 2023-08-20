3f8d2bda48c5886e06afeeb818407eda757bd007
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-20 00:41:29 -0300

    fix(storage): validate if window exist on local storage hook

--- src/localStorageHook.ts
+++ src/localStorageHook.ts
@@ -1,31 +1,35 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const useLocalStorage = (key: string, initialValue: any) => {
   const [state, setState] = useState(() => {
     // Initialize the state
     try {
-      const value = window.localStorage.getItem(key)
-      // Check if the local storage already has any values,
-      // otherwise initialize it with the passed initialValue
-      return value ? JSON.parse(value) : initialValue
+      if (typeof window !== "undefined") {
+        const value = window.localStorage.getItem(key);
+        // Check if the local storage already has any values,
+        // otherwise initialize it with the passed initialValue
+        return value ? JSON.parse(value) : initialValue;
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  })
+  });
 
   const setValue = (value: (arg0: any) => any) => {
     try {
       // If the passed value is a callback function,
       //  then call it with the existing state.
-      const valueToStore = value instanceof Function ? value(state) : value
-      window.localStorage.setItem(key, JSON.stringify(valueToStore))
-      setState(value)
+      const valueToStore = value instanceof Function ? value(state) : value;
+      if (typeof window !== "undefined") {
+        window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        setState(value);
+      }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
-  return [state, setValue]
-}
+  return [state, setValue];
+};
 
-export default useLocalStorage
+export default useLocalStorage;
