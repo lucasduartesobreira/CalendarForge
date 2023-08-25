ef122e8fb64c76c0edabd9f4d3c9a2dd99e64845
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-25 16:29:22 -0300

    refactor(hooks): better hook using effects

--- src/localStorageHook.ts
+++ src/localStorageHook.ts
@@ -1,35 +1,37 @@
-import { useState } from "react";
+/* eslint-disable no-console */
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useState, useEffect } from "react";
 
-const useLocalStorage = (key: string, initialValue: any) => {
-  const [state, setState] = useState(() => {
-    // Initialize the state
-    try {
-      if (typeof window !== "undefined") {
-        const value = window.localStorage.getItem(key);
-        // Check if the local storage already has any values,
-        // otherwise initialize it with the passed initialValue
-        return value ? JSON.parse(value) : initialValue;
-      }
-    } catch (error) {
-      console.log(error);
-    }
-  });
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [storedValue, setStoredValue] = useState<T | undefined>();
 
-  const setValue = (value: (arg0: any) => any) => {
-    try {
-      // If the passed value is a callback function,
-      //  then call it with the existing state.
-      const valueToStore = value instanceof Function ? value(state) : value;
-      if (typeof window !== "undefined") {
-        window.localStorage.setItem(key, JSON.stringify(valueToStore));
-        setState(value);
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const setValue = (value: T) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
   };
 
-  return [state, setValue];
+  useEffect(() => {
+    const value = window.localStorage.getItem(key);
+
+    if (value) {
+      try {
+        const parsed = JSON.parse(value) as T;
+        setStoredValue(parsed);
+      } catch (error) {
+        console.log(error);
+        setStoredValue(initialValue);
+      }
+    } else {
+      setStoredValue(initialValue);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (storedValue) {
+      setValue(storedValue);
+    }
+  }, [storedValue]);
+
+  return [storedValue as T, setStoredValue] as const;
 };
 
 export default useLocalStorage;
