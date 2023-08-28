1903c3fadbbd3fff16e0fc57bdcf622dca358de3
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-28 19:41:33 -0300

    feat(hook): create a data hook to generate the storages

---
+++ src/hooks/dataHook.ts
@@ -0,0 +1,45 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+import { CalendarEvent, EventStorage } from "@/services/events/events";
+import { useMap } from "@/hooks/mapHook";
+import { createContext, useEffect, useMemo, useState } from "react";
+import { None, Some, Option } from "@/utils/option";
+import { Calendar, CalendarStorage } from "@/services/calendar/calendar";
+
+type Storages = {
+  eventsStorage: EventStorage;
+  calendarsStorage: CalendarStorage;
+};
+
+const StorageContext = createContext<Option<Storages>>(None());
+
+export function useDataStorage(): Option<Storages> {
+  const eventsHook = useMap<string, CalendarEvent>("eventsMap");
+  const calendarsHook = useMap<string, Calendar>("calendars");
+  const [clientData, setClientData] = useState<Option<Storages>>(None());
+
+  const hasWindow = typeof window !== "undefined";
+  const eventsHookExists = eventsHook.isSome();
+  const calendarsHookExists = calendarsHook.isSome();
+
+  useEffect(() => {
+    if (eventsHook.isSome() && calendarsHook.isSome()) {
+      const [eventsMap, eventsActions] = eventsHook.unwrap();
+      const eventsStorage = new EventStorage(eventsMap, eventsActions);
+
+      const [calendarsMap, calendarsActions] = calendarsHook.unwrap();
+      const calendarsStorage = new CalendarStorage(
+        calendarsMap,
+        calendarsActions,
+      );
+      setClientData(Some({ eventsStorage, calendarsStorage }));
+    }
+  }, [hasWindow, eventsHookExists, calendarsHookExists]);
+
+  const memoized = useMemo(() => {
+    return clientData;
+  }, [clientData]);
+
+  return memoized;
+}
+
+export { StorageContext };
