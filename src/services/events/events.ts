c33524a2b6eaa6186b5ce88169e342bc26082c70
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-08-28 10:58:06 -0300

    feat(events): add calendar storage

--- src/services/events/events.ts
+++ src/services/events/events.ts
@@ -1,3 +1,6 @@
+import { Actions } from "@/hooks/mapHook";
+import { Err, Ok, Result } from "@/utils/result";
+
 type CalendarEvent = {
   id: string;
   startDate: number;
@@ -7,4 +10,74 @@ type CalendarEvent = {
   calendar_id: string;
 };
 
+class EventStorage {
+  private events: Map<string, CalendarEvent>;
+  private actions: Actions<string, CalendarEvent>;
+
+  constructor(
+    events: Map<string, CalendarEvent>,
+    actions: Actions<string, CalendarEvent>,
+  ) {
+    this.events = events;
+    this.actions = actions;
+  }
+
+  add(event: Omit<CalendarEvent, "id">): Result<CalendarEvent, null> {
+    const eventWithId = { id: "a", ...event };
+    this.actions.set(eventWithId.id, eventWithId);
+    return Ok(eventWithId);
+  }
+
+  remove(eventId: string): Result<CalendarEvent, symbol> {
+    const event = this.events.get(eventId);
+    if (event == undefined) {
+      return Err(Symbol("Event not found"));
+    }
+
+    this.actions.remove(eventId);
+
+    return Ok(event);
+  }
+
+  findById(eventId: string): Result<CalendarEvent, symbol> {
+    const event = this.events.get(eventId);
+    if (event == undefined) {
+      return Err(Symbol("Event not found"));
+    }
+
+    return Ok(event);
+  }
+
+  find(predicate: (event: CalendarEvent) => boolean) {
+    for (const event of this.events.values()) {
+      if (predicate(event)) {
+        return Ok(event);
+      }
+    }
+
+    return Err(Symbol("Event not found"));
+  }
+
+  update(eventId: string, event: Partial<Omit<CalendarEvent, "id">>) {
+    const eventFound = this.events.get(eventId);
+    if (eventFound == undefined) {
+      return Err(Symbol("Event not found"));
+    }
+
+    const newEvent: CalendarEvent = {
+      id: eventId,
+      title: event.title ?? eventFound.title,
+      endDate: event.endDate ?? eventFound.endDate,
+      startDate: event.startDate ?? eventFound.startDate,
+      calendar_id: event.calendar_id ?? eventFound.calendar_id,
+      description: event.description ?? eventFound.description,
+    };
+
+    this.actions.set(eventId, newEvent);
+
+    return Ok(newEvent);
+  }
+}
+
 export type { CalendarEvent };
+export { EventStorage };
