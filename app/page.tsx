9cc2b862111d13782dc2202f889f05157ae4c4b7
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-09-01 18:09:34 -0300

    feat(calendars): week walker

--- app/page.tsx
+++ app/page.tsx
@@ -20,10 +20,36 @@ const Home = () => {
     setStartDate(firstDayOfTheWeek);
   }, []);
 
+  const endDate = new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000 - 1);
   return (
     <StorageContext.Provider value={data}>
       <main className="h-full flex flex-col">
-        <div className="flex-none h-[48px] bg-red-500">Navbar</div>
+        <div className="flex-none grid grid-cols-[auto_15%] grid-rows-[100%] relative h-[48px] bg-red-500">
+          <div className="col-start-1">Navbar</div>
+          <div className="col-start-2 grid grid-rows-[100%] grid-cols-[auto] place-items-center gap-[4px] w-[100%] h-[100%]">
+            <button
+              onClick={() => {
+                setStartDate(
+                  new Date(startDate.getTime() - 7 * 24 * 60 * 60 * 1000),
+                );
+              }}
+              className="col-start-1"
+            >
+              &lt;
+            </button>{" "}
+            <div className="text-center col-start-2">{`${startDate.getDate()}/${startDate.getMonth()} - ${endDate.getDate()}/${endDate.getMonth()}`}</div>{" "}
+            <button
+              onClick={() => {
+                setStartDate(
+                  new Date(startDate.getTime() + 7 * 24 * 60 * 60 * 1000),
+                );
+              }}
+              className="col-start-3"
+            >
+              &gt;
+            </button>
+          </div>
+        </div>
         <div className="flex overflow-hidden">
           <SideBar className="w-[15%]" />
           <div className="w-[85%] max-h-[100%]">
