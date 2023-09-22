6d2b2e18288699beb14630e7b8f37560371f04e4
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-09-22 19:29:57 -0300

    feat(boards): add a new board button

--- app/components/boards/boards.tsx
+++ app/components/boards/boards.tsx
@@ -2,9 +2,12 @@ import { StorageContext } from "@/hooks/dataHook";
 import { Project } from "@/services/projects/projectsStorage";
 import { Option } from "@/utils/option";
 import { PropsWithChildren, useContext } from "react";
+import { Board } from "@/services/boards/boards";
 
 export default function Container({ children }: PropsWithChildren) {
-  return <div className="flex">{children}</div>;
+  return (
+    <div className="flex h-full w-full bg-gray-100 relative">{children}</div>
+  );
 }
 
 function ProjectBoards({ project }: { project: Option<Project> }) {
@@ -16,19 +19,46 @@ function ProjectBoards({ project }: { project: Option<Project> }) {
         () => null,
         (project) => {
           const boards = boardsStorage.allBoardsFromProject(project.id);
-          return boards.map((board, index) => (
-            <div key={index} className="bg-white w-[360px] h-full">
-              {board.title}
-            </div>
-          ));
+          return (
+            <>
+              {boards.map((board, index) => (
+                <Board key={index} board={board}></Board>
+              ))}
+              <AddBoard project={project}></AddBoard>
+            </>
+          );
         },
       );
     },
   );
 }
 
-function Board({}) {
-  return <div className="bg-white">project</div>;
+function AddBoard({ project }: { project: Project }) {
+  const { storages } = useContext(StorageContext);
+  return (
+    <div className="h-full w-[32px] absolute right-0">
+      <div className="relative h-full">
+        <button
+          className="absolute top-1/2 h-[32px] w-[32px] rounded-full bg-blue-500 text-white"
+          onClick={() => {
+            storages.map(({ boardsStorage }) => {
+              boardsStorage.add({ title: "New Board", project_id: project.id });
+            });
+          }}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function Board({ board }: { board: Board }) {
+  return (
+    <div className="bg-white h-full text-black">
+      <input value={board.title} />
+    </div>
+  );
 }
 
 export const Boards = {
