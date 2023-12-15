95ea8af13200b8241b13821305ab93ff4ab8d455
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-12-14 21:17:27 -0300

    feat(events): create recurring event manager to deal with updating and removing recurring events

--- src/services/events/events.ts
+++ src/services/events/events.ts
@@ -66,14 +66,15 @@ type RecurringSettings = (
 
 type StopConfig =
   | {
+      type: "frequency";
       afterFrequency: number;
     }
-  | { afterDay: Date };
+  | { type: "date"; afterDay: Date };
 
 type CreateEvent = AddValue<CalendarEvent>;
 type UpdateEvent = UpdateValue<CalendarEvent>;
 
-export const GetEventDays = (
+export const getRecurringDates = (
   startDate: Date,
   recurringSettings: RecurringSettings,
 ): Date[] => {
@@ -129,7 +130,7 @@ export const GetEventDays = (
 
     const daysSet = new Set(days.sort());
 
-    if ("afterFrequency" in stop) {
+    if (stop.type === "frequency") {
       let { afterFrequency } = stop;
 
       let dates = new Array();
@@ -199,6 +200,19 @@ export const GetEventDays = (
   }
 
   return [];
+};
+
+export const getCappedRecurringSetting = (
+  capDate: Date,
+  recurringSettings: RecurringSettings,
+): RecurringSettings => {
+  const capDateAtMidnight = new Date(capDate.getTime()).setHours(0, 0, 0, 0);
+  const updatedAfterDay = new Date(capDateAtMidnight);
+
+  return {
+    ...recurringSettings,
+    stop: { type: "date", afterDay: updatedAfterDay },
+  };
 };
 
 export class EventStorageIndexedDb
@@ -279,7 +293,7 @@ export class EventStorageIndexedDb
 
     const bulkResult = created.map(({ id, ...event }) => {
       if (event.recurring_settings != null) {
-        const dates = GetEventDays(
+        const dates = getRecurringDates(
           new Date(event.startDate),
           event.recurring_settings,
         ).slice(1);
@@ -415,6 +429,471 @@ export class EventStorageIndexedDb
 
   bulk(initialValue?: CalendarEvent[]): Bulk<CalendarEvent> {
     return new Bulk(initialValue ?? [], this.map);
+  }
+}
+
+export class RecurringEventsManager {
+  map: StorageActions<"id", CalendarEvent>;
+
+  constructor(map: StorageActions<"id", CalendarEvent>) {
+    this.map = map;
+  }
+
+  private hasChanged({
+    event,
+    eventFound,
+  }: {
+    event: UpdateEvent;
+    eventFound: CalendarEvent;
+  }) {
+    const {
+      startDate: oldStartDate,
+      endDate: oldEndDate,
+      recurring_settings: oldRecurringSettings,
+      color: oldColor,
+      title: oldTitle,
+      task_id: oldTaskId,
+      todo_id: oldTodoId,
+      calendar_id: oldCalendarId,
+      description: oldDescription,
+      notifications: oldNotifications,
+    } = eventFound;
+    const {
+      startDate: newStartDate,
+      endDate: newEndDate,
+      recurring_settings: newRecurringSettings,
+      color: newColor,
+      title: newTitle,
+      task_id: newTaskId,
+      todo_id: newTodoId,
+      calendar_id: newCalendarId,
+      description: newDescription,
+      notifications: newNotifications,
+    } = event;
+    const changedStartDate = oldStartDate !== newStartDate;
+    const changedEndDate = oldEndDate !== newEndDate;
+    const changedFrequencyType =
+      oldRecurringSettings?.frequencyType !==
+      newRecurringSettings?.frequencyType;
+    const changedStopType =
+      oldRecurringSettings?.stop.type !== newRecurringSettings?.stop.type;
+    const changedDailyFrequency =
+      oldRecurringSettings?.frequencyType === "daily" &&
+      newRecurringSettings?.frequencyType === "daily" &&
+      oldRecurringSettings.frequency !== newRecurringSettings.frequency;
+
+    const changedWeekDays =
+      oldRecurringSettings?.frequencyType === "weekly" &&
+      newRecurringSettings?.frequencyType === "weekly" &&
+      oldRecurringSettings.days.find(
+        (value, index) => newRecurringSettings.days.at(index) !== value,
+      ) != null;
+
+    const changedStopFrequency =
+      oldRecurringSettings?.stop.type === "frequency" &&
+      newRecurringSettings?.stop.type === "frequency" &&
+      oldRecurringSettings.stop.afterFrequency !==
+        newRecurringSettings.stop.afterFrequency;
+    const changedStopDate =
+      oldRecurringSettings?.stop.type === "date" &&
+      newRecurringSettings?.stop.type === "date" &&
+      oldRecurringSettings.stop.afterDay !== newRecurringSettings.stop.afterDay;
+
+    const changedColor = oldColor !== newColor;
+    const changedTitle = oldTitle !== newTitle;
+    const changedTaskId = oldTaskId !== newTaskId;
+    const changedTodoId = oldTodoId !== newTodoId;
+    const changedCalendarId = oldCalendarId !== newCalendarId;
+    const changedDescription = oldDescription !== newDescription;
+
+    const changedNotificationsLength =
+      oldNotifications.length !== newNotifications?.length;
+    const changedNotificationsContent =
+      oldNotifications.find(
+        (value, index) =>
+          value.id !== newNotifications?.at(index)?.id ||
+          value.from !== newNotifications.at(index)?.from ||
+          value.time !== newNotifications.at(index)?.time ||
+          value.timescale !== newNotifications.at(index)?.timescale,
+      ) != null;
+    const changedNotifications =
+      changedNotificationsLength || changedNotificationsContent;
+
+    return {
+      changedStartDate,
+      changedEndDate,
+      changedRecurringSettings:
+        changedFrequencyType ||
+        changedStopType ||
+        changedDailyFrequency ||
+        changedWeekDays ||
+        changedStopFrequency ||
+        changedStopDate,
+      changedDateUnrelated:
+        changedColor ||
+        changedTitle ||
+        changedTaskId ||
+        changedTodoId ||
+        changedCalendarId ||
+        changedDescription ||
+        changedNotifications,
+
+      changedUnrelatedSpecific: {
+        changedColor,
+        changedTitle,
+        changedTaskId,
+        changedTodoId,
+        changedCalendarId,
+        changedDescription,
+        changedNotifications,
+      },
+    };
+  }
+
+  private newDateWithReferenceHours(
+    timeInMiliseconds: number,
+    reference: Date,
+  ) {
+    return new Date(timeInMiliseconds).setHours(
+      reference.getHours(),
+      reference.getMinutes(),
+      reference.getSeconds(),
+      reference.getMilliseconds(),
+    );
+  }
+
+  private updateOnBulk(
+    events: CalendarEvent[],
+    updateEvent: UpdateEvent,
+    referenceStart: Date,
+    referenceEnd: Date,
+    baseBulk: Bulk<CalendarEvent>,
+  ) {
+    const bulk = events.reduce(
+      (
+        bulk,
+        { id, startDate: startDateMiliseconds, endDate: endDateMiliseconds },
+      ) => {
+        const startDate = this.newDateWithReferenceHours(
+          startDateMiliseconds,
+          referenceStart,
+        );
+        const endDate = this.newDateWithReferenceHours(
+          endDateMiliseconds,
+          referenceEnd,
+        );
+
+        bulk.update({
+          ...updateEvent,
+          id,
+          startDate: startDate,
+          endDate: endDate,
+        });
+        return bulk;
+      },
+      baseBulk,
+    );
+
+    return bulk;
+  }
+
+  private removeFromBulk(
+    events: CalendarEvent["id"][],
+    bulk: Bulk<CalendarEvent>,
+  ) {
+    events.forEach((id) => bulk.delete(id));
+
+    return bulk;
+  }
+
+  private addToBulk(
+    events: Omit<CalendarEvent, "id">[],
+    bulk: Bulk<CalendarEvent>,
+  ) {
+    events.forEach((event) => bulk.insert(event));
+
+    return bulk;
+  }
+
+  private fixedUpdate(
+    event: UpdateEvent,
+    changedSpecific: {
+      changedTodoId: boolean;
+      changedColor: boolean;
+      changedTitle: boolean;
+      changedTaskId: boolean;
+      changedCalendarId: boolean;
+      changedDescription: boolean;
+      changedNotifications: boolean;
+      changedRecurringSettings: boolean;
+    },
+  ) {
+    const {
+      changedTodoId,
+      changedColor,
+      changedTitle,
+      changedTaskId,
+      changedCalendarId,
+      changedDescription,
+      changedNotifications,
+      changedRecurringSettings,
+    } = changedSpecific;
+    const toUpdate: UpdateEvent = {};
+    if (changedTodoId) toUpdate.todo_id = event.todo_id;
+    if (changedColor) toUpdate.color = event.color;
+    if (changedTitle) toUpdate.title = event.title;
+    if (changedTaskId) toUpdate.task_id = event.task_id;
+    if (changedCalendarId) toUpdate.calendar_id = event.calendar_id;
+    if (changedDescription) toUpdate.description = event.description;
+    if (changedNotifications) toUpdate.notifications = event.notifications;
+    if (changedRecurringSettings)
+      toUpdate.recurring_settings = event.recurring_settings;
+
+    return toUpdate;
+  }
+
+  async updateForward(
+    id: CalendarEvent["id"],
+    event: UpdateEvent,
+  ): Promise<R.Result<null, symbol>> {
+    if (event.recurring_settings != null) {
+      const found = await this.map.findById(id);
+      return await found
+        .map(async (eventFound) => {
+          const {
+            changedStartDate,
+            changedEndDate,
+            changedRecurringSettings,
+            changedDateUnrelated,
+            changedUnrelatedSpecific,
+          } = this.hasChanged({
+            eventFound,
+            event,
+          });
+
+          if (
+            !changedStartDate &&
+            !changedEndDate &&
+            !changedRecurringSettings &&
+            !changedDateUnrelated
+          )
+            return R.Ok(null);
+
+          const { startDate: oldStartDate, endDate: oldEndDate } = eventFound;
+          const {
+            startDate: newStartDate,
+            endDate: newEndDate,
+            recurring_settings: newRecurringSettings,
+            recurring_id,
+          } = event;
+
+          const foundAllRelatedEvents = await this.map.findAll({
+            recurring_id: recurring_id ?? id,
+          });
+
+          if (!foundAllRelatedEvents.find(({ id: thisId }) => thisId === id)) {
+            foundAllRelatedEvents.splice(0, 0, eventFound);
+          }
+
+          const allNextEvents = foundAllRelatedEvents.filter(
+            ({ startDate }) => startDate >= oldStartDate,
+          );
+
+          const baseBulk = this.map.bulk(allNextEvents);
+
+          const referenceStartDate = new Date(newStartDate ?? oldStartDate);
+          const referenceEndDate = new Date(newEndDate ?? oldEndDate);
+
+          const dataToUpdate = this.fixedUpdate(event, {
+            ...changedUnrelatedSpecific,
+            changedRecurringSettings,
+          });
+
+          if (!changedRecurringSettings) {
+            const bulk = this.updateOnBulk(
+              allNextEvents,
+              dataToUpdate,
+              referenceStartDate,
+              referenceEndDate,
+              baseBulk,
+            );
+
+            return await bulk.commit();
+          } else if (newRecurringSettings != null) {
+            const getNewRecurringDates = new Set(
+              getRecurringDates(
+                new Date(oldStartDate),
+                newRecurringSettings,
+              ).map((date) => date.getTime()),
+            );
+
+            const [kept, keptDates, removed] = allNextEvents.reduce(
+              (acc, event) => {
+                const { startDate, id } = event;
+                if (getNewRecurringDates.has(startDate)) {
+                  acc[0].push(event);
+                  acc[1].add(startDate);
+                } else {
+                  acc[2].push(id);
+                }
+                return acc;
+              },
+              [new Array(), new Set([]), new Array()] as [
+                CalendarEvent[],
+                Set<CalendarEvent["startDate"]>,
+                CalendarEvent["id"][],
+              ],
+            );
+
+            const newEvents = Array.from(getNewRecurringDates.values())
+              .filter((dateToTest) => !keptDates.has(dateToTest))
+              .map((startDateMiliseconds) => {
+                const startDate = this.newDateWithReferenceHours(
+                  startDateMiliseconds,
+                  referenceStartDate,
+                );
+                return {
+                  ...eventFound,
+                  ...event,
+                  recurring_id: id,
+                  startDate: startDate,
+                  endDate:
+                    startDate +
+                    referenceEndDate.getTime() -
+                    referenceStartDate.getTime(),
+                };
+              });
+
+            const bulkRemoved = this.removeFromBulk(removed, baseBulk);
+            const bulkWithUpdated = this.updateOnBulk(
+              kept,
+              { ...dataToUpdate, recurring_id: id },
+              referenceStartDate,
+              referenceEndDate,
+              bulkRemoved,
+            );
+
+            bulkWithUpdated.update({
+              id,
+              recurring_id: undefined,
+              ...dataToUpdate,
+            });
+            const bulkComplete = this.addToBulk(newEvents, bulkWithUpdated);
+
+            return await bulkComplete.commit();
+          } else if (newRecurringSettings == null) {
+            const allNext = allNextEvents.reduce(
+              (acc, { id: thisId }) => {
+                if (thisId !== id) acc.push(thisId);
+                return acc;
+              },
+              [] as CalendarEvent["id"][],
+            );
+            const bulk = this.removeFromBulk(allNext, baseBulk);
+            bulk.update({
+              id,
+              recurring_id: undefined,
+              recurring_settings: undefined,
+            });
+
+            return await bulk.commit();
+          } else {
+            return R.Ok(null);
+          }
+        })
+        .ok(Symbol("Cannot update deleted event"))
+        .asyncFlatten();
+    }
+
+    return R.Ok(null);
+  }
+
+  async updateAll(
+    id: CalendarEvent["id"],
+    event: UpdateEvent,
+  ): Promise<R.Result<null, symbol>> {
+    const findRoot = await this.map.findById(id);
+    return await findRoot
+      .map(({ id: thisId }) => this.updateForward(thisId, event))
+      .ok(Symbol("Cannot update a deleted record"))
+      .asyncFlatten();
+  }
+
+  async deleteForward(id: CalendarEvent["id"]) {
+    const found = await this.map.findById(id);
+    return found
+      .map(async (eventFound) => {
+        const { recurring_settings } = eventFound;
+        if (recurring_settings != null) {
+          const base = eventFound.recurring_id
+            ? await this.map.findById(eventFound.recurring_id)
+            : O.Some(eventFound);
+
+          return await base
+            .ok(Symbol("Cannot find main event"))
+            .map(async (base) => {
+              const allRelatedEventsFound = await this.map.findAll({
+                recurring_id: eventFound.recurring_id ?? id,
+              });
+
+              if (allRelatedEventsFound.find(({ id }) => id === base.id))
+                allRelatedEventsFound.splice(0, 0, base);
+
+              const allForward = allRelatedEventsFound.reduce(
+                (acc, nextEvent) => {
+                  const { id, startDate } = nextEvent;
+                  if (startDate > eventFound.startDate) {
+                    acc.set(id, nextEvent);
+                  }
+                  return acc;
+                },
+                new Map() as Map<CalendarEvent["id"], CalendarEvent>,
+              );
+
+              const bulk = this.map.bulk(allRelatedEventsFound);
+              const removedFromBulk = this.removeFromBulk(
+                Array.from(allForward.keys()),
+                bulk,
+              );
+
+              const allBefore = allRelatedEventsFound.filter(
+                (relatedEvent) => !allForward.has(relatedEvent.id),
+              );
+
+              const cappedRecurringSettings = getCappedRecurringSetting(
+                new Date(allBefore.at(-1)?.startDate ?? eventFound.startDate),
+                recurring_settings,
+              );
+
+              return allBefore.reduce((bulk, event) => {
+                bulk.update({
+                  id: event.id,
+                  recurring_settings: cappedRecurringSettings,
+                });
+                return bulk;
+              }, removedFromBulk);
+            })
+            .map(async (bulk) => (await bulk).commit())
+            .asyncFlatten();
+        }
+        return R.Ok(null);
+      })
+      .ok(Symbol("Couldn't find record with this id"))
+      .asyncFlatten();
+  }
+
+  async deleteAll(id: CalendarEvent["id"]) {
+    const eventFound = await this.map.findById(id);
+    return eventFound
+      .ok(Symbol("Couldn't find record with this id"))
+      .map(({ recurring_id, recurring_settings, id }) =>
+        recurring_settings != null
+          ? id === recurring_id || recurring_id == null
+            ? this.deleteForward(id)
+            : this.deleteForward(recurring_id)
+          : (async () => R.Ok(null))(),
+      )
+      .asyncFlatten();
   }
 }
 
