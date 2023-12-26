3522d3cf089a3d23778f3b4b63afa43dcf96ede0
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2023-12-26 18:48:53 -0300

    feat(buttons): add a tertiary button

--- components/shared/button-view/buttons.tsx
+++ components/shared/button-view/buttons.tsx
@@ -50,7 +50,45 @@ const ButtonPrimary = ({
   );
 };
 
+const ButtonTertiary = ({
+  value,
+  sizeType,
+  innerRef,
+  disabled,
+  ...props
+}: HTMLButtonExtended<{
+  value: string;
+  sizeType: "sm" | "base" | "md" | "lg" | "xl";
+  innerRef?: RefObject<any>;
+}> & { ref?: never }) => {
+  const size =
+    sizeType === "xl"
+      ? "rounded-xl shadow-xl"
+      : sizeType === "sm"
+      ? "rounded-sm shadow-sm"
+      : sizeType === "base"
+      ? "rounded shadow"
+      : sizeType === "md"
+      ? "rounded-md shadow-md"
+      : sizeType === "lg"
+      ? "rounded-lg shadow-lg"
+      : "";
+
+  return (
+    <button
+      {...props}
+      className={`${disabled ? "opacity-40" : ""} ${
+        props.className
+      } bg-white text-primary-500 border-2 border-primary-500 ${size}`}
+      ref={innerRef}
+    >
+      {value}
+    </button>
+  );
+};
+
 export const Button = {
   Primary: ButtonPrimary,
   Secondary: ButtonSecondary,
+  Tertiary: ButtonTertiary,
 };
