59f3e808971aa23c62f6bd2408a678849ae91d7f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-01-13 21:04:46 -0300

    feat(editor): add visual clue for selected events

--- components/shared/flexible-view/flexibleView.tsx
+++ components/shared/flexible-view/flexibleView.tsx
@@ -4,6 +4,7 @@ import { PropsWithChildren, useContext, useEffect, useState } from "react";
 import { DayViewContent } from "../day-view/dayContent";
 import { DayDropZone } from "../day-view/dayBackground";
 import { DraggedEvent } from "../day-view/dayEventsContent";
+import { SelectedRefs } from "@/components/calendar-editor-week-view/contexts";
 
 export const AcceptedDaysValue = [1, 2, 3, 4, 5, 6, 7] as const;
 export type ViewSize = (typeof AcceptedDaysValue)[number];
@@ -62,7 +63,7 @@ export const FlexibleView = ({
 
   return (
     <div
-      className={`${style} grid ${weekGridClass} grid-row-1 overflow-scroll`}
+      className={`${style} grid ${weekGridClass} grid-row-1 overflow-scroll relative`}
       id={id}
     >
       {children}
@@ -96,6 +97,84 @@ export const FlexibleView = ({
           </DayViewContent.DayContainer>
         ),
       )}
+      <Selection />
     </div>
   );
+};
+
+const Selection = () => {
+  const selectedRefs = useContext(SelectedRefs);
+  const [dimensions, setDimensions] = useState({
+    x_start: Number.MAX_SAFE_INTEGER,
+    x_end: Number.MIN_SAFE_INTEGER,
+    y_start: Number.MAX_SAFE_INTEGER,
+    y_end: Number.MIN_SAFE_INTEGER,
+  });
+
+  useEffect(() => {
+    selectedRefs.map(([refs]) => {
+      const container = document.getElementById("calendar-week-container");
+      const containerBound = container?.getBoundingClientRect();
+      if (containerBound == null) {
+        return;
+      }
+
+      const containerLeftPadding = containerBound.left;
+      const containerTopPadding = containerBound.top;
+      const containerScroll = container?.scrollTop ?? 0;
+
+      const { x_start, x_end, y_start, y_end } = Array.from(
+        refs.values(),
+      ).reduce(
+        (acc, div) => {
+          const currentBound = div.current?.getBoundingClientRect();
+          if (currentBound == null) return acc;
+
+          return {
+            x_start:
+              currentBound.left < acc.x_start ? currentBound.left : acc.x_start,
+            x_end:
+              currentBound.right > acc.x_end ? currentBound.right : acc.x_end,
+            y_start:
+              currentBound.top < acc.y_start ? currentBound.top : acc.y_start,
+            y_end:
+              currentBound.bottom > acc.y_end ? currentBound.bottom : acc.y_end,
+          };
+        },
+        {
+          x_start: Number.MAX_SAFE_INTEGER,
+          x_end: Number.MIN_SAFE_INTEGER,
+          y_start: Number.MAX_SAFE_INTEGER,
+          y_end: Number.MIN_SAFE_INTEGER,
+        },
+      );
+
+      setDimensions({
+        x_start: x_start - containerLeftPadding,
+        x_end: x_end - containerLeftPadding,
+        y_start: y_start + containerScroll - containerTopPadding,
+        y_end: y_end + containerScroll - containerTopPadding,
+      });
+    });
+  }, [selectedRefs]);
+
+  if (
+    dimensions.x_start !== Number.MAX_SAFE_INTEGER &&
+    dimensions.x_end !== Number.MIN_SAFE_INTEGER &&
+    dimensions.y_start !== Number.MAX_SAFE_INTEGER &&
+    dimensions.y_end !== Number.MIN_SAFE_INTEGER
+  )
+    return (
+      <div
+        className="bg-transparent border-2 border-dashed border-amber-400 absolute pointer-events-none rounded-lg"
+        style={{
+          top: dimensions.y_start - 4,
+          left: dimensions.x_start - 4,
+          width: Math.abs(dimensions.x_end - dimensions.x_start) + 4 + 4,
+          height: Math.abs(dimensions.y_start - dimensions.y_end) + 4 + 4,
+        }}
+      />
+    );
+
+  return null;
 };
