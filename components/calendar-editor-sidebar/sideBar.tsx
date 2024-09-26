42c09f3d05cc702214225a7c584026d297010694
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-26 11:53:45 -0300

    feat: add to both editor and normal sidebar a mode card

--- components/calendar-editor-sidebar/sideBar.tsx
+++ components/calendar-editor-sidebar/sideBar.tsx
@@ -80,10 +80,32 @@ export const EditorSideBar = (
   const refButton = useRef(null);
 
   return (
-    <div {...arg} className={`${arg.className}`}>
+    <div
+      {...arg}
+      className={`${arg.className} flex flex-col min-w-fit gap-1 flex-none`}
+    >
       {storages.isSome() && calendars.isSome() && actions.isSome() && (
         <>
-          <MiniCalendar className="p-1 first:mb-1" />
+          <div
+            className={`inline-flex items-center justify-center w-full p-1 min-w-fit bg-white rounded-xl shadow-lg border-[1px] border-neutral-200 overflow-hidden text-neutral-600`}
+          >
+            <span className="m-2 text-lg text-neutral-600 select-none">
+              {"Mode"}
+            </span>
+            <label className="relative inline-flex items-center cursor-pointer ml-auto mr-2">
+              <input
+                type="checkbox"
+                className="sr-only peer"
+                checked={true}
+                onChange={() => {}}
+              />
+              <div className="w-10 h-5 border-primary-100 border-[2px] peer-focus:outline-none rounded-full transition-all ease-out peer peer-checked:after:start-[24px] peer-checked:after:border-primary-300 peer-checked:after:bg-primary-300 after:content-[''] after:absolute after:top-[4px] after:start-[4px] after:bg-primary-200 after:border-primary-200 after:border after:rounded-full after:h-3 after:w-3 after:transition-all peer-checked:bg-primary-200" />
+              <span className="ml-1 text-sm text-neutral-600 select-none">
+                {"Editor"}
+              </span>
+            </label>
+          </div>
+          <MiniCalendar className="p-1" />
           <ListContainer
             titleSection={<Titles.Normal name="Calendars" />}
             buttonSection={
