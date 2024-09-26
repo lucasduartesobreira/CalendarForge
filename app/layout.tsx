df2022bb776be9a0d78b37e2106bf2249fba61c3
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-26 10:52:29 -0300

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={inter.className}>{children}</body>
     </html>
   )
